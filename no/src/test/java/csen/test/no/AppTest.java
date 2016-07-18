8f1231219a80a35f173bde17537424366376810d
ssyoo <ssyoo@gtone.co.kr>
2016-07-18 20:54:44 +0900

    second

---
+++ no/src/test/java/csen/test/no/AppTest.java
@@ -0,0 +1,42 @@
+package csen.test.no;
+
+
+import javax.crypto.Cipher;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+    extends TestCase
+{
+    /**
+     * Create the test case
+     *
+     * @param testName name of the test case
+     */
+    public AppTest( String testName )
+    {
+        super( testName );
+        int key =Cipher.PRIVATE_KEY;
+    }
+
+    /**
+     * @return the suite of tests being tested
+     */
+    public static Test suite()
+    {
+        return new TestSuite( AppTest.class );
+    }
+
+    /**
+     * Rigourous Test :-)
+     */
+    public void testApp()
+    {
+        assertTrue( true );
+    }
+}
